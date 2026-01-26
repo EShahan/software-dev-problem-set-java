9d139a088b61b6b349dea0c5e0e863f97a540205
Eric Shahan <ctibalt@gmail.com>
2026-01-26 09:53:23 -0600

    Hashset Solution for problem #1

--- src/main/java/ContainsDuplicate.java
+++ src/main/java/ContainsDuplicate.java
@@ -1,12 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
     static boolean containsDuplicate(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
+
+        HashSet<Integer> repeat = new HashSet<>();
+        for (int num : nums) {
+            if (repeat.contains(num)) {
+                return true;
             }
+            repeat.add(num);
         }
         return false;
+
+//          Brute Force Solution
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[i] == nums[j]) {
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
     }
 }
