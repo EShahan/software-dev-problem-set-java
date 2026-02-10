8c865be19b9a2e317e2b79f0a51f6a92320b7f0f
Eric Shahan <ctibalt@gmail.com>
2026-02-10 11:02:32 -0600

    Hashmap solution for problem #3

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -1,15 +1,35 @@
+import java.util.HashMap;
+
 public class TwoSum {
-
-
-    //Brute Force Solution.
+    //Hashmap solution. We only need to find if there's a matching difference in the list.
     static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        HashMap<Integer, Integer> indices = new HashMap<>();
+
+        //Convert array to Hashmap
+        for (int i = 0; i < nums.length; i++) {
+            indices.put(nums[i], i);
+        }
+
+        //Iterate over list to find potential differences, then check against hashmap for matches. If there's a match return the indices.
+        //If there's no matches in any loop return null.
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (indices.containsKey(complement) && indices.get(complement) != i) {
+                return new int[]{i, indices.get(complement)};
             }
         }
         return null;
     }
+
+    //Brute Force Solution.
+//    static int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < nums.length - 1; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[i] + nums[j] == target) {
+//                    return new int[]{i, j};
+//                }
+//            }
+//        }
+//        return null;
+//    }
 }
